aa6fa104c08eba83aa42fe58f19f5b38af7ab491
Pamela Fava <20021037@studenti.unipo.it>
2020-01-30 12:14:55 +0100

    Modifiche

---
+++ app/src/main/java/it/uniupo/infobooks/util/Constants.java
@@ -0,0 +1,10 @@
+package it.uniupo.infobooks.util;
+
+public class Constants {
+
+    public static final String BOOK = "book";
+    public static final String DATASET = "dataset";
+    public static final String IS_UPDATE = "isUpdate";
+    public static final String MAPS = "maps";
+    public static final String VISIBILITY = "visibility";
+}
